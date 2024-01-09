df1f2d59ada19279d924e39214bdd4e8d6f3ed43
mitul88 <shahed.rahman88@gmail.com>
2024-01-10 00:35:06 +0600

    login with cookie and jwt token done

---
+++ src/app/api/user/login/route.js
@@ -0,0 +1,23 @@
+import { CreateToken } from "@/app/utility/JWTTokenHelper";
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+export async function POST(req, res) {
+    try {
+        let reqBody = await req.json()
+        const prisma = new PrismaClient();
+        const result = await prisma.user.findUnique({
+            where: reqBody
+        })
+        if(!result) {
+            return NextResponse.json({status: 'no user found'})
+        } else {
+            let token=await CreateToken(result['email'], result['id'])
+            let expireDuration=new Date(Date.now()+24*60*60*1000)
+            const cookieString = `token=${token};expires=${expireDuration.toUTCString()};path=/`
+            return NextResponse.json({status:"success", data: token}, {status:200, headers: {'set-cookie': cookieString}})  
+        }
+    } catch(e) {
+        return NextResponse.json({status:"failed", data: e})
+    }
+}
