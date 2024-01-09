7214f5344dbd2a8c2cb9fe4cbd4d7bf5b99804d3
mitul88 <shahed.rahman88@gmail.com>
2024-01-09 23:28:07 +0600

    model, jwt helper, registration done

---
+++ prisma/migrations/20240103135553_all_models_migrate/migration.sql
@@ -0,0 +1,100 @@
+-- CreateTable
+CREATE TABLE `categories` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(100) NOT NULL,
+    `createdAt` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+    `updatedAt` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+
+    UNIQUE INDEX `categories_name_key`(`name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `news_list` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(100) NOT NULL,
+    `short_des` VARCHAR(400) NOT NULL,
+    `img1` VARCHAR(300) NOT NULL,
+    `img2` VARCHAR(300) NOT NULL,
+    `img3` VARCHAR(300) NOT NULL,
+    `img4` VARCHAR(300) NOT NULL,
+    `keywords` VARCHAR(300) NOT NULL,
+    `long_des` LONGTEXT NOT NULL,
+    `type` VARCHAR(200) NOT NULL,
+    `catId` INTEGER NOT NULL,
+    `createdAt` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+    `updatedAt` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `user` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `firstName` VARCHAR(50) NOT NULL,
+    `lastName` VARCHAR(50) NOT NULL,
+    `email` VARCHAR(50) NOT NULL,
+    `mobile` VARCHAR(50) NOT NULL,
+    `password` VARCHAR(50) NOT NULL,
+    `otp` VARCHAR(50) NOT NULL,
+    `createdAt` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+    `updatedAt` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+
+    UNIQUE INDEX `user_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `comment` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `userId` INTEGER NOT NULL,
+    `postId` INTEGER NOT NULL,
+    `createdAt` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+    `updatedAt` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `socials` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `facebook` VARCHAR(200) NOT NULL,
+    `youtube` VARCHAR(200) NOT NULL,
+    `twitter` VARCHAR(200) NOT NULL,
+    `linkedin` VARCHAR(200) NOT NULL,
+    `about` TEXT NOT NULL,
+    `address` TEXT NOT NULL,
+    `createdAt` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+    `updatedAt` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `subscribe` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(200) NOT NULL,
+    `createdAt` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+    `updatedAt` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+
+    UNIQUE INDEX `subscribe_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `policies` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `long_des` LONGTEXT NOT NULL,
+    `type` VARCHAR(50) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `news_list` ADD CONSTRAINT `news_list_catId_fkey` FOREIGN KEY (`catId`) REFERENCES `categories`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `comment` ADD CONSTRAINT `comment_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `user`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `comment` ADD CONSTRAINT `comment_postId_fkey` FOREIGN KEY (`postId`) REFERENCES `news_list`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ src/app/api/user/registration/route.js
@@ -0,0 +1,16 @@
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+export async function POST(req, res) {
+    try {
+        let reqBody = await req.json()
+        reqBody.otp = "0";
+        const prisma = new PrismaClient();
+        const result = await prisma.user.create({
+            data: reqBody
+        })
+        return NextResponse.json({status:"success", data: result})
+    } catch(e) {
+        return NextResponse.json({status:"failed", data: e})
+    }
+}
