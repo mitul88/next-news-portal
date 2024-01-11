4e223251c046e3710cc530bc2973fbf800ffd5aa
mitul88 <shahed.rahman88@gmail.com>
2024-01-11 14:39:38 +0600

    user update done

---
+++ src/app/api/user/profile/update/route.js
@@ -0,0 +1,22 @@
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+import { headers } from 'next/headers';
+
+
+export async function POST(req, res) {
+    try {
+        let headerList = headers();
+        let id = parseInt(headerList.get('id'));
+
+        let reqBody = await req.json()
+
+        const prisma = new PrismaClient();
+        const result = await prisma.user.update({
+            where:{id: id},
+            data: reqBody
+        })
+        return NextResponse.json({status:"success", data: result})
+    } catch(e) {console.log(e)
+        return NextResponse.json({status:"failed", data: e})
+    }
+}
